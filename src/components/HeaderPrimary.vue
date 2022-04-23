cccb3c885a3b185df5f9e5b628ad41affba0d4d6
glegorry <glegorry@hotmail.com>
2022-04-23 18:09:27 -0300

    Header e Footer

---
+++ src/components/HeaderPrimary.vue
@@ -0,0 +1,38 @@
+<template>
+  <header>
+    <nav>
+      <route-link class="logo">
+        <img
+          src="@/assets/ranek.svg"
+          alt="Ranek"
+        >
+      </route-link>
+      <route-link
+        class="btn"
+        to="/login"
+      >Vender / Login</route-link>
+    </nav>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "HeaderPrimary"
+}
+</script>
+
+<style scoped>
+nav {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 15px 20px;
+  box-shadow: 0 2px 4px rgba(30, 60, 90, 0.1);
+}
+.logo {
+  padding: 10px 0;
+}
+.logo img {
+  width: 90px;
+}
+</style>
