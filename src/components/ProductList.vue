76619486928ab3e25f58d9d85ebde4a0ffa0ef0d
glegorry <glegorry@hotmail.com>
2022-04-23 21:56:56 -0300

    lista de produtos

---
+++ src/components/ProductList.vue
@@ -0,0 +1,39 @@
+<template>
+  <section>
+    <div
+      v-for="produto in produtos"
+      :key="produto.id"
+    >
+      <img
+        v-if="produto.fotos"
+        :src="produto.fotos[0].src"
+        :alt="produto.fotos[0].titulo"
+      >
+      <p class="preco">{{produto.preco}}</p>
+      <h2 class="titulo">{{produto.nome}}</h2>
+      <p>{{produto.decricao}}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      produtos: null
+    }
+  },
+  methods: {
+    getProdutos () {
+      fetch("https://localhost:3000/produto")
+        .then(res => res.josn())
+        .then(res => {
+          this.produtos = res
+        })
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,28 @@
 <template>
   <div>
+    <h1>👍 Compre ou Venda 👎</h1>
+    <ProductSearch />
+    <ProductList />
   </div>
 </template>
 
 <script>
+import ProductSearch from '@/components/ProductSearch.vue'
+import ProductList from '@/components/ProductList.vue'
 
 export default {
   name: 'HomeView',
   components: {
+    ProductSearch,
+    ProductList
   }
 }
 </script>
+<style scoped>
+h1 {
+  color: #87f;
+  margin-top: 40px;
+  font-size: 2rem;
+  text-align: center;
+}
+</style>
