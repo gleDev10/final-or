f48c27b07cbc560235b85c170d52ee7a882fdf90
glegorry <glegorry@hotmail.com>
2022-04-24 16:56:25 -0300

    CSS campo de busca

--- src/components/ProductSearch.vue
+++ src/components/ProductSearch.vue
@@ -5,6 +5,7 @@
       name="busca"
       id="busca"
       v-model="busca"
+      placeholder="Buscar..."
     >
     <input
       type="submit"
@@ -50,5 +51,31 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+form {
+  max-width: 600px;
+  margin: 30px auto 60px;
+  position: relative;
+}
+#busca {
+  width: 100%;
+  padding: 20px;
+  border: none;
+}
+#busca:focus,
+#busca:hover {
+  transform: scale(1.1);
+}
+#lupa {
+  width: 62px;
+  height: 62px;
+  background-image: url("../assets/search.svg") no-repeat center center;
+  text-indent: -150px;
+  border: none;
+  cursor: pointer;
+  position: absolute;
+  top: 0;
+  right: 0;
+  box-shadow: none;
+}
 </style>
